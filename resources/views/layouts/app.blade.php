26f6524b84c16356fc48e20416f2e6a020fd7697
towakey <kyosgame@gmail.com>
2025-11-10 23:46:08 +0900

    chore: Livewire ESMモジュールファイルを追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'PersonalManager') }}</title>
-    <script src="https://cdn.tailwindcss.com"></script>
     @vite('resources/css/app.css')
     @livewireStyles
 </head>
